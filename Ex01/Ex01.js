d2516f4e0f48be5f765fa323d05d013955410708
bangseungmin <bang3957@gmail.com>
2018-10-24 00:11:13 +0900

    ES6 1일차 블록스코프

---
+++ Ex01/Ex01.js
@@ -0,0 +1,35 @@
+{
+    let a = 10
+    {
+        let a = 20
+        console.log(a)
+    }
+    console.log(a)
+}
+console.log(a) 
+
+function hasValue (p) {
+    console.log(v)
+    if (p) {
+        var v = 'blue'
+        console.log(v)
+    } else {
+        let v = 'red'
+        console.log(v)
+    }
+    console.log(v)
+}
+hasValue(10)
+
+function hasValue2 (p) {
+    console.log(v)  
+    if (p) {
+        let v = 'blue'
+        console.log(v)
+    } else {
+        let v = 'red'
+        console.log(v)
+    }
+    console.log(v) 
+}
+hasValue2(10)
