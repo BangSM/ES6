13aaba10a24850977a062145bb731ba500a8d8ab
bangseungmin <bang3957@gmail.com>
2018-10-24 00:12:09 +0900

    ES6 1일차 블록스코프

--- Ex01/Ex01.js
+++ Ex01/Ex01.js
@@ -33,3 +33,37 @@ function hasValue2 (p) {
     console.log(v) 
 }
 hasValue2(10)
+
+(function(){
+    var a = 10;
+    (function(){
+        console.log(a); 
+        var a = 20;
+    })();
+    console.log(a); 
+})();
+console.log(a); 
+
+if(true){
+    let a = 10
+    if(true){
+        console.log(a) 
+        const a = 20
+    }
+    console.log(a) 
+}
+console.log(a) 
+
+var value = 0;
+var obj = {
+    value : 1, 
+    setValue : function (){
+        this.value = 2;     
+        (function (){
+            this.value = 3; 
+        })();
+    }
+}
+obj.setValue();
+console.log(value);    
+console.log(obj.value);
